5bb95d136d30e9ed88219a753fbfba277afd376b
Pranam Shaw <vea5a.pranam05@gmail.com>
2025-09-16 18:46:59 +0530

    Hero Section complete

--- frontend/src/Pages/Hero.jsx
+++ frontend/src/Pages/Hero.jsx
@@ -1,10 +1,76 @@
 import React from 'react'
+import { Zap, Shield, UsersRound,ArrowRight } from 'lucide-react'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
+
+  useGSAP(()=>{
+    gsap.from(".Ai", {
+      y:-50,
+      opacity:0,
+      duration:0.5,
+    })
+    gsap.from(".heading", {
+      y:-50,
+      opacity:0,
+      duration:0.5,
+    })
+    gsap.from(".paragraph", {
+      x:100,
+      opacity:0,
+      duration:0.5,
+    })
+    gsap.from(".buttons", {
+      y:50,
+      opacity:0,
+      duration:0.5
+    })
+    
+    gsap.from(".cards .card1", {
+      y:-50,
+      duration:0.5,
+      opacity:0,
+      stagger:0.2,
+    })
+  }, [])
+
   return (
-    <div>
-      <div>
-          
+    <div className='w-full max-md:h-[1200px] h-[92.5vh] bg-[#F7F5FF] max-md:mb-30'>
+      <div className='flex flex-col items-center gap-10 '>
+          <div className='Ai flex gap-1 bg-[#e1defa] max-md:px-3 max-md:text-[15px] font-medium px-10 justify-center rounded-full py-1 mt-20'>
+              <Zap /> <p>Ai Powered Prompt Management</p> 
+          </div>
+          <div>
+            <h1 className='heading text-5xl max-sm:text-3xl max-md:text-4xl max-md:max-w-sm font-semibold max-w-lg text-center mx-auto mb-5 mt-5'>Build Your Perfect <span className='text-[#9E92E8]'>Prompt Library</span></h1>
+            <p className='paragraph text-xl max-w-[750px] text-center opacity-70 max-sm:text-[14px] max-sm:max-w-[300px] max-md:text-[16px] max-md:max-w-[400px] mx-auto'>Create, Organize, and share Ai prompts with a professional community. Keep up your best prompts private to contribute to the global knowledge base</p>
+          </div>
+          <div className='buttons flex gap-5'>
+            <Link to="/login">
+            <button className='px-7 py-3 bg-[#9E92E8] text-xl text-white font-medium rounded-md cursor-pointer border-1 hover:bg-transparent hover:text-black border-[#9E92E8] transition-all duration-100 flex gap-1 max-md:text-[16px] max-md:px-4 max-sm:text-[14px] max-sm:px-2'>Get Started Free <ArrowRight className='max-sm:hidden' /></button>
+            </Link>
+            <Link to="/community">
+            <button className='px-7 py-3 border-1 transition-all duration-100 border-gray-400  text-xl hover:bg-[#9E92E8] hover:border-[#9E92E8] hover:text-white font-medium rounded-md cursor-pointer max-md:text-[16px] max-md:px-4 max-sm:text-[14px] max-sm:px-2' >Explore Community</button>
+            </Link>
+          </div>
+          <div className='cards flex justify-center items-center text-center gap-20 max-2xl:gap-10 mt-10 flex-wrap '>
+            <div className="card1">
+            <Shield className='w-9 h-9 mx-auto mb-4 mt-5' color='#9E92E8'/>  
+            <h3 className='text-2xl font-medium max-md:text-xl mb-1'>Private & Secure</h3>
+            <p className='text-lg opacity-70 max-w-[300px] text-center max-lg:text-[14px] max-lg:max-w-[200px]'>Keep your prompts private or share with the community</p>
+            </div>
+            <div className="card1">
+            <Zap className='w-9 h-9 mx-auto mb-4 mt-5' color='#9E92E8'/>  
+            <h3 className='text-2xl font-medium max-md:text-xl mb-3'>Smart Categories</h3>
+            <p className='text-lg opacity-70 max-w-[300px] text-center max-lg:text-[14px] max-lg:max-w-[200px]'>Automatically categorized by task type and purpose</p>
+            </div>
+            <div className="card1">
+            <UsersRound className='w-9 h-9 mx-auto mb-4 mt-5' color='#9E92E8'/>  
+            <h3 className='text-2xl font-medium max-md:text-xl mb-1'>Community Driven</h3>
+            <p className='text-lg opacity-70 max-w-[300px] text-center mx-auto max-lg:text-[14px] max-lg:max-w-[200px]'>Learned from the best prompts shared by professionals</p>
+            </div>
+          </div>
       </div>
     </div>
   )
--- frontend/src/Pages/Navbar.jsx
+++ frontend/src/Pages/Navbar.jsx
@@ -1,98 +1,139 @@
-import React from "react";
-import { BookOpen, Plus, Menu, X } from "lucide-react";
-import { Link } from "react-router-dom";
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
+  import React, {useState} from "react";
+  import { BookOpen, Plus, Menu, X } from "lucide-react";
+  import { Link } from "react-router-dom";
+  import gsap from 'gsap';
+  import { useGSAP } from '@gsap/react';
 
-const Navbar = () => {
-  return (
-    <>
-      <div className="flex justify-around  border-y-2 border-zinc-200 max-lg:hidden p-3">
-        <div className="flex items-center gap-2">
-          <div className="bg-[#9089E5] p-2 inline-block rounded-md">
-            <BookOpen color="#ffffff" />
-          </div>
-          <h3 className="text-xl font-semibold">
-            Prompt<span className="text-[#9089e5]">Vault</span>
-          </h3>
-        </div>
-        <div className="flex gap-5 font-medium items-end">
-          <Link to="/dashboard" className="text-lg">
-            Dashboard
-          </Link>
-          <Link to="/community" className="text-lg">
-            Community
-          </Link>
-          <a href="#features" className="text-lg">
-            Features
-          </a>
-        </div>
-        <div className="flex gap-2">
-          <Link
-            to="/login"
-            className="text-lg  bg-[#9089E5] px-6 py-2 text-white rounded-md "
-          >
-            Login
-          </Link>
-          <Link
-            to="/add-prompt"
-            className="flex text-md items-center bg-zinc-100 px-6 py-2 rounded-md border-1 border-zinc-300"
-          >
-            <Plus />
-            Add Prompt
-          </Link>
-        </div>
-      </div>
+  const Navbar = () => {
 
-      {/* Mobile Navbar */}
-      <div className="lg:hidden">
-        <div className="lg:hidden p-3 flex justify-between px-10 items-center">
+    useGSAP(()=>{
+      gsap.from(".logo", {
+        y:-50,
+        x:-50,
+        opacity:0,
+        duration:0.7,
+      })
+      gsap.from(".logoHeading", {
+        y:-50,
+        x:-50,
+        opacity:0,
+        duration:0.7,
+      })
+      gsap.from(".button", {
+        y:-50,
+        x:50,
+        opacity:0,
+        duration:0.7,
+      })
+      gsap.from(".Navbar a", {
+        y:-50,
+        opacity:0,
+        duration:0.5,
+        stagger:0.2,
+      })
+    })
+
+    const [visible, setIsVisible] = useState(false)
+
+    return (
+      <>
+        <div className="flex justify-around  border-y-2 border-zinc-200 max-lg:hidden p-3">
           <div className="flex items-center gap-2">
-            <div className="bg-[#9089E5] p-2 inline-block rounded-md">
-              <BookOpen color="#ffffff" />
+            <Link to="/" className="flex gap-2 items-center">
+            <div className="logo bg-[#9089E5] p-2 inline-block rounded-md">
+              <BookOpen className="" color="#ffffff" />
             </div>
-            <h3 className="text-xl font-semibold">
+            <h3 className="logoHeading text-xl font-semibold">
               Prompt<span className="text-[#9089e5]">Vault</span>
             </h3>
+            </Link>
           </div>
-          <Menu />
-        </div>
-        <div
-          className={`flex flex-col gap- bg-[#9089E5] shadow-2xl  h-screen w-full gap-5 p-10  text-white absolute top-0 left-[100%]`}
-        >
-          <div className="flex gap-5 flex-col font-medium   ">
-            <Link to="/dashboard" className="text-xl">
+          <div className="Navbar flex gap-5 font-medium items-end">
+            <Link to="/" className="text-lg">
+              Home
+            </Link>
+            <Link to="/dashboard" className="text-lg">
               Dashboard
             </Link>
-            <Link to="/community" className="text-xl">
+            <Link to="/community" className="text-lg">
               Community
             </Link>
-            <a href="#features" className="text-xl">
+            <a href="#features" className="text-lg">
               Features
             </a>
           </div>
-          <div className="flex gap-5 flex-col">
+          <div className="flex gap-2">
             <Link
               to="/login"
-              className="text-lg  bg-[#fff] text-black w-30 font-semibold text-center  px-6 py-2  rounded-md "
+              className="button text-lg  bg-[#9089E5] px-6 py-2 text-white rounded-md "
             >
               Login
             </Link>
             <Link
               to="/add-prompt"
-              className="flex text-md items-center bg-zinc-100 px-6 py-2 w-50 text-black rounded-md border-1 border-zinc-300"
+              className="button flex text-md items-center bg-zinc-100 px-6 py-2 rounded-md border-1 border-zinc-300"
             >
               <Plus />
               Add Prompt
             </Link>
-            <div className=" bg-white w-7 absolute top-10 items-center rounded-full flex justify-center left-90 h-7">
-              <X className="cursor-pointer " color="black" />
+          </div>
+        </div>
+
+        {/* Mobile Navbar */}
+        <div className="lg:hidden">
+          <div className="lg:hidden p-3 flex justify-between px-10 items-center">
+            <div className="flex items-center gap-2">
+                <Link to="/" className="flex items-center gap-1">
+              <div className="bg-[#9089E5] p-2 inline-block rounded-md">
+                <BookOpen color="#ffffff" />
+              </div>
+              <h3 className="text-xl font-semibold">
+                Prompt<span className="text-[#9089e5]">Vault</span>
+              </h3>
+              </Link>
+            </div>
+              
+            <Menu className="cursor-pointer" onClick={()=>{setIsVisible(true)}}/>
+          </div>
+          <div
+            className={`navBar ${visible ? "left-[30%]" : "left-[100%]" } flex flex-col gap- bg-[#9089E5] shadow-2xl  h-screen w-full gap-5 p-10  text-white fixed top-0 transition-all duration-500 z-50`}
+          >
+            <div className="flex gap-5 flex-col font-medium ">
+              <Link to="/" onClick={()=>{setIsVisible(false)}} className="text-xl">
+                Home
+              </Link>
+              <Link to="/dashboard" onClick={()=>{setIsVisible(false)}} className="text-xl">
+                Dashboard
+              </Link>
+              <Link to="/community" onClick={()=>{setIsVisible(false)}} className="text-xl">
+                Community
+              </Link>
+              <a href="#features" onClick={()=>{setIsVisible(false)}} className="text-xl">
+                Features
+              </a>
+            </div>
+            <div className="flex gap-5 flex-col">
+              <Link
+                to="/login"
+                className="text-lg  bg-[#fff] text-black w-30 font-semibold text-center  px-6 py-2  rounded-md "
+              >
+                Login
+              </Link>
+              <Link
+                to="/add-prompt"
+                className="flex text-md items-center bg-zinc-100 px-6 py-2 w-50 text-black rounded-md border-1 border-zinc-300"
+              >
+                <Plus />
+                Add Prompt
+              </Link>
+              <div className=" bg-white w-7 items-center rounded-full flex justify-center h-7" onClick={()=>{setIsVisible(false)}}>
+                <X className="cursor-pointer " color="black" />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  );
-};
+      </>
+    );
+  };
 
-export default Navbar;
+  export default Navbar;
