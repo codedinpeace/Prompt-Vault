22fedb5b5191392bff27fe3d050bcb78e1cbad64
Pranam Shaw <vea5a.pranam05@gmail.com>
2025-09-22 18:24:36 +0530

    dd

--- backend/api/utils/generate.token.js
+++ backend/api/utils/generate.token.js
@@ -3,7 +3,12 @@ const jwt = require('jsonwebtoken')
 const genToken = (userId, res)=>{
     try {
         const token =  jwt.sign({userId}, process.env.JWT_SECRET_KEY_GENERATOR, {expiresIn:"1d"})
-        res.cookie("token", token)
+        res.cookie("jwt", token, {
+            httpOnly: true,
+            secure: true,    // only sent over HTTPS
+            sameSite: "None", // allows cross-site requests
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 1 week
+          });
         return token
     } catch (error) {
         res.status(400).json({message:"Something went wrong while creating the token"})
