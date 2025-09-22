b8cc78e256623f0c99082bd0e27ccbd6e71fc885
Pranam Shaw <vea5a.pranam05@gmail.com>
2025-09-22 17:57:20 +0530

    simon sit

--- frontend/src/Utils/axios.js
+++ frontend/src/Utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const axiosInstance = axios.create({
-    baseURL:"http://localhost:3000/api",
+    baseURL:"https://prompt-vault-1ckl.onrender.com/api",
     withCredentials:true,
 })  
